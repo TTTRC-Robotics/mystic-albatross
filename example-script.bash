1ca7b1c49dcb7e3666c7fb2205fd27a05bf3f63a
Florent Chretien <flopubs@gmail.com>
2023-10-13 08:26:03 +0600

    add script

---
+++ example-script.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo
+echo "Hello $USER,"
+echo
+echo "Welcome to this demo script, that you made executable"
+echo
+echo "I wish you the best for the MysticAlbatross and hope you will enjoy it!"
+echo 
+echo "Cheers!"
